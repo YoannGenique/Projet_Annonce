700678000604ed06b728dd0902a646a578e3056d
Yoann Génique <genique.yoann@outlook.com>
2021-01-15 12:39:11 +0100

    maj

--- Website_Ressources/form_m.php
+++ Website_Ressources/form_m.php
@@ -4,53 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cactuu</title>
+    <!-- Mon CSS -->
+    <link rel="stylesheet" href="assets/CSS/style.css">
 </head>
 <body>
     <!-- header -->
     <header>
         <!-- En Tête de la Page -->
-        <div>
+        <div class="head_all">
             <!-- Titre en Img Oh! Cactuu -->
-            <img src="" alt="">
+            <div class="logo">
+                <img src="assets/img/logo.png" alt="Logo oh ! cactuu !" width="100%" height="100%">
+            </div>
         </div>
     </header>
     <!-- Fin header -->
     <!-- Main -->
     <main>
         <section>
-            <div>
+            <div class="div_first_btn">
                 <!-- Btn Retour au cactuu -->
-                <button></button>
+                <button class="first_btn">Retour au CACTUu</button>
             </div>
         </section>
         <section>
             <div>
                 <!-- Formulaire de modification -->
                 <form action="" enctype="" method="">
-                    <div>
-                        <div>
+                    <div class="form">
+                        <div class="in">
                             <!-- Nom du Cactuu -->
-                            <label for=""></label>
-                            <input type="text">
+                            <label class="lab" for="nom">Nom du CACTUu:</label><br>
+                            <input class="champ" type="text" name="nom">
                         </div>
-                        <div>
+                        <div class="in">
                             <!-- email du revendeur -->
-                            <label for=""></label>
-                            <input type="text">
+                            <label class="lab" for="email">Email:</label><br>
+                            <input class="champ" type="email" name="email">
                         </div>
-                        <div>
+                        <div class="in"> 
                             <!-- téléphone du revendeur -->
-                            <label for=""></label>
-                            <input type="text">
+                            <label class="lab" for="telephone">Téléphone:</label><br>
+                            <input class="champ" type="text" name="telephone">
                         </div>
-                        <div>
+                        <div class="in">
                             <!-- photo du Cactuu -->
-                            <label for=""></label>
-                            <input type="text">
+                            <label class="lab" for="img">Photo du CACTUu:</label><br>
+                            <input class="champ_img" type="file" name="img"> 
                         </div> 
-                        <div>
+                        <div class="div_first_btn">
                             <!-- Btn enregistrer l'annonce -->
-                            <button></button>
+                            <button class="first_btn">Enregistrer</button>
                         </div>
                     </div>
                 </form>
@@ -58,8 +62,11 @@
         </section>
     </main>
     <!-- Fin Main -->
+    <!-- PUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUSHHHH -->
+    <DIV class="push_f"></DIV>
+    <!-- PUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUSHHHH -->
     <!-- footer -->
-    <footer>
+    <footer class="foo">
     </footer>
     <!-- Fin footer -->
 </body>
